c9321a76df92d13ece1a6a34ba9a094a4f99a90c
kmturhan <kmturhan@gmail.com>
2018-04-20 00:13:03 +0300

    Frontend geliştirme

--- Frontend/source/javascripts/general.js
+++ Frontend/source/javascripts/general.js
@@ -187,5 +187,17 @@ $(function() {
    stickySidebar('.sticky-four', '.sticky-stopper-four')
 
 
+
+
+   $(function() {
+    $('#phone').on('keydown', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||(/65|67|86|88/.test(e.keyCode)&&(e.ctrlKey===true||e.metaKey===true))&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
+    $("#phone").attr("maxlength", 11);
+});
+
+ 
+        
+     
+
+
 });
 

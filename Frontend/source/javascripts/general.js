5f970d74bfbb7472cf9e237985fe6701a816fa90
batuhangoksucs <batuhan.goksu@ph.com.tr>
2018-04-20 16:25:07 +0300

    frontend geliştirm
    anasayfa product alanı güncellendi

--- Frontend/source/javascripts/general.js
+++ Frontend/source/javascripts/general.js
@@ -211,8 +211,6 @@ $(function() {
    stickySidebar('.sticky-four', '.sticky-stopper-four')
 
 
-<<<<<<< HEAD
-
 
    $(function() {
     $('#phone').on('keydown', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||(/65|67|86|88/.test(e.keyCode)&&(e.ctrlKey===true||e.metaKey===true))&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
@@ -221,10 +219,6 @@ $(function() {
 
  
         
-     
-
-
-=======
 $(window).scroll(function() {
         var scrollTop = $(window).scrollTop()
         var divOffset = parseInt($('.animatable').offset().top / 2)
@@ -241,6 +235,5 @@ $('.products-container-content-right-item__title').click(function(){
   $(this).next('.products-container-content-right-item__text').fadeIn();
 });
 
->>>>>>> 775540bc56c72dbde04777adb3ef855577ba676e
 });
 

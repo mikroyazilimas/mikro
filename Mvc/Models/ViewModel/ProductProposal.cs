137efcdfc56b54eb12535130a03ac46f228b9ec1
Aykut Sarıdede <aykut.saridede@ph.com.tr>
2018-04-27 16:58:00 +0300

    dfcvvc
    cvvc

---
+++ Mvc/Models/ViewModel/ProductProposal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitefinityWebApp.Mvc.Models.ViewModel
+{
+    public class ProductProposal
+    {
+        public string Product { get; set; }
+    }
+}

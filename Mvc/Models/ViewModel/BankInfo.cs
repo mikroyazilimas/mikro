ef506e83d28aebd9684020e74d53245b3da8727d
kmturhan <kmturhan@gmail.com>
2018-06-20 14:23:36 +0300

    Mvc Geliştirme

---
+++ Mvc/Models/ViewModel/BankInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitefinityWebApp.Mvc.Models.ViewModel
+{
+    public class BankInfoGroup
+    {
+        public string GroupName { get; set; }
+        public List<BankInfoItem> Items { get; set; }
+    }
+
+    public class BankInfoItem
+    {
+        public string BranchCode { get; set; }
+        public string IBAN { get; set; }
+        public string MainTitle { get; set; }
+    }
+}
